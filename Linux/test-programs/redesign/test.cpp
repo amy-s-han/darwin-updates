e954669a14263631556c18e273d4f402f9206626
Amy Han <ahan2@swarthmore.edu>
2015-07-07 11:06:50 -0400

    a few more tests for darwin

--- Linux/test-programs/redesign/test.cpp
+++ Linux/test-programs/redesign/test.cpp
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <unistd.h>
 #include "darwinController.h"
 
 #define MAXNUM_TXPARAM 	(256)
@@ -19,21 +20,50 @@ int main(int argc, char** argv){
 		return 0;
 	}
 
+	usleep(5000);
 
+	printf("~~~ Testing MakeBulkPacket ~~~\n");
     darCon->MakeBulkPacket(darCon->BulkReadTxPacket);
 
+    printf("~~~ Testing Read ~~~\n");
 	unsigned char rxpacket[MAXNUM_RXPARAM + 10] = {0, };
 	unsigned char txpacketread[] = {0, 0, 0x14, 0x04, 0x02, 0x24, 0x02, 0};
 
 	darCon->FinishPacket(txpacketread);
 
     int result = darCon->ReadWrite(txpacketread, rxpacket);
+    int word;
 
-	printf("result: %d\n", result);
+	if(result == 0){
+		printf("Failed read! \n");
+	} else {
+		word = darCon->MakeWord((int)rxpacket[PARAMETER], (int)rxpacket[PARAMETER + 1]);
+    	printf("Successful read. Motor position: %d\n\n", word);
 
-    int word = darCon->MakeWord((int)rxpacket[PARAMETER], (int)rxpacket[PARAMETER + 1]);
-    printf("Motor position: %d\n\n", word);
+	}
 
+	printf("~~~ Testing Write ~~~\n");
+
+	
+	while(result == 0){
+		//usleep(5000);
+		result = darCon->ReadWrite(txpacketread, rxpacket);
+	}
+	
+	word += 200;
+    int value_low = darCon->GetLowByte(word);
+    int value_high = darCon->GetHighByte(word);
+
+	unsigned char txpacketwrite[] = {0, 0, 0x05, 0x05, 0x03, 0x1E, 0, 0, 0};
+	txpacketwrite[PARAMETER+1] = value_low;
+	txpacketwrite[PARAMETER+2] = value_high;
+
+	darCon->FinishPacket(txpacketwrite);
+
+	int result2 = darCon->ReadWrite(txpacketwrite, rxpacket);
+	printf("result2: %d\n", result2);
+
+    
 	printf("Press ENTER to close port\n");
 	getchar();
 
