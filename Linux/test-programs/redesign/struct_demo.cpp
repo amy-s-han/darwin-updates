edba7a4741298c14889385129dc2a88e90fe8019
Dan Rovner <drovner1@swarthmore.edu>
2015-07-07 10:26:18 -0400

    First draft of struct demo

--- Linux/test-programs/redesign/struct_demo.cpp
+++ Linux/test-programs/redesign/struct_demo.cpp
@@ -3,18 +3,20 @@
 struct JointData {
 	uint8_t  flags;
 	uint16_t goal;
-	uint16_t p, i, d;
+	uint8_t p, i, d;
 };
 
 enum {
 	NUM_JOINTS = 20,
 	FLAG_GOAL_CHANGED = 0x01,
-	FLAG_P_CHANGED = 0x02,
-	FLAG_I_CHANGED = 0x04,
-	FLAG_D_CHANGED = 0x08,
+	FLAG_GAINS_CHANGED = 0x02,
 	FLAG_ENABLE = 0x80,
 };
 
+
+// if just goal changed, write 2 bytes
+// if just gains chamnged write 3 bytes
+// if both changed write all 5 bytes
 void foo() {
 	
 	// declare an array of joint data
@@ -42,19 +44,65 @@ void foo() {
 
 	// at this point can tell how many bytes per motor to send
 
+
 	uint8_t buf[MAX_PACKET_LENGTH*MAX_NUM_PACKETS + HEADER_LEN + FOOTER_LEN];
 	int buf_offset = 0;
+	unsigned char numparams = 0;
+	unsigned char lenparam;
+	unsigned char txpacket[128] = {0, };
 
 	// todo: fill in header
 
-	for (int i=0; i<NUM_JOINTS; ++i) {
-		// TODO fill in buf with packets
-		const JointData& ji = joints[i];
-		bool enabled = ji.flags & FLAG_ENABLE; // pick off top bit to see if joint enabled
-		uint8_t changed = ji.flags & ~FLAG_ENABLE; // pick off bottom 7 bits
-		if (enabled && changed) { // if this motor is enabled and has new data
+    if (change_flags == FLAG_GOAL_CHANGED){  // If any of Goals changed but none of the PIDs changed
+    	lenparam = 2;
+		for (int i=0; i<NUM_JOINTS; ++i) {   
+			JointData& ji = joints[i];
+			if (ji.flags == FLAG_ENABLE + FLAG_GOAL_CHANGED){    // If this joint's Goal has changed, add to sync write
+				buf[buf_offset++] = i;
+				buf[buf_offset++] = GetLowByte(goal);
+				buf[buf_offset++] = GetHighByte(goal);
+				ji.flags = FLAG_ENABLE; // cleared the changed bits cause we are about to send this
+				numparams += 3;
+			}
 		}
-	}
+    }
+
+    else if (change_flags == FLAG_GAINS_CHANGED){ // If any of the gains changed but none of the Goals changed
+    	lenparam = 3;
+		for (int i=0; i<NUM_JOINTS; ++i) {   
+			JointData& ji = joints[i];
+			if (ji.flags == FLAG_ENABLE + FLAG_GAINS_CHANGED){    // If this joint's gains have changed, add to sync write
+				buf[buf_offset++] = i;
+				buf[buf_offset++] = ji.d;
+				buf[buf_offset++] = ji.i;
+				buf[buf_offset++] = ji.p;
+				ji.flags = FLAG_ENABLE; // cleared the changed bits cause we are about to send this
+				numparams += 4;
+			}
+		}
+    }
+
+    else if (change_flags == FLAG_GAINS_CHANGED + FLAG_GOAL_CHANGED){ // If both PID and Goal have changed
+    	lenparam = 5;
+		for (int i=0; i<NUM_JOINTS; ++i) {   
+			JointData& ji = joints[i];
+			if (ji.flags & FLAG_GOAL_CHANGED || ji.flags & FLAG_GAINS_CHANGED  && ji.flags & FLAG_ENABLE){    // If this joint's Goal has changed, add to sync write
+				buf[buf_offset++] = i;
+				buf[buf_offset++] = ji.d;
+				buf[buf_offset++] = ji.i;
+				buf[buf_offset++] = ji.p;
+				buf[buf_offset++] = GetLowByte(goal);
+				buf[buf_offset++] = GetHighByte(goal);
+				ji.flags = FLAG_ENABLE; // cleared the changed bits cause we are about to send this
+				numparams += 6;
+			}
+		}
+    }
+
+    SyncWrite(txpacket, 0x03, buf, numparams, lenparam);
+    port->ClearPort();
+    port->WritePort(txpacket, numparams + 8);
+
 
 	// todo: fill in checksum
 
